203c125f60e52879de33c4a77527c32f06565add
Erderson Lucas <erdersonlucas05@gmail.com>
2023-09-07 17:41:25 -0300

    finalizado e erro corrigido

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,11 @@ document.addEventListener('DOMContentLoaded', function() {
             evento.preventDefault();
             let numeroMaximo = document.getElementById('numero-maximo').value;
             numeroMaximo = parseInt(numeroMaximo);
-            
+
             let numeroAleatorio = Math.random() * numeroMaximo;
+            numeroAleatorio = Math.floor(numeroAleatorio + 1);
 
             document.getElementById('resultado-valor').innerText = numeroAleatorio;
+            document.querySelector('.resultado').style.display = 'block';
         })
 })
